2f1ba4546f02e69dc211172b145683267f28312d
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-08-26 18:26:07 +0530

    add category controller with create and get all categories functionality

---
+++ src/controllers/categories.controller.js
@@ -0,0 +1,43 @@
+import { categoryModel } from "../models/category.model";
+
+export const createCategory = async (req, res) => {
+  try {
+    const { name, slug } = req.body;
+    if (!name || !slug)
+      return res
+        .status(401)
+        .send({ message: "All fields are required!", success: false });
+    const isExistCategory = await categoryModel.findOne({
+      $or: [{ name }, { slug }],
+    });
+    if (isExistCategory)
+      return res
+        .status(401)
+        .send({ message: "Category is already exist!", success: false });
+    const createCategory = await categoryModel.create({
+      name,
+      slug,
+    });
+    return res
+      .status(200)
+      .send({ message: "Category Created.", createCategory });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .send({ message: "Internal Server Error.", success: false, error });
+  }
+};
+
+export const getAllCategories = async (req, res) => {
+  try {
+    const allCategories = await categoryModel.find();
+    if (allCategories.length == 0)
+      return res
+        .status(404)
+        .send({ message: "Sorry! No Categories were found!", success: false });
+    return res
+      .status(200)
+      .send({ message: "Categories.", allCategories, success: true });
+  } catch (error) {}
+};
