d82c4386197d84051a4e917998a8824e8028cd23
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-09-03 15:13:20 +0530

    add getAllVariants functionality with caching and improve createVariant error handling

--- src/controllers/variant.controller.js
+++ src/controllers/variant.controller.js
@@ -1,6 +1,7 @@
-import {productModel} from "../models/product.model.js"
-import {variantModel} from "../models/variant.model.js"
-import {v2 ass uuid} from "uuid";
+import { productModel } from "../models/product.model.js";
+import { variantModel } from "../models/variant.model.js";
+import { v2 as uuid } from "uuid";
+import redis from "../config/redis/redis.js";
 
 export const createVariant = async (req, res) => {
   try {
@@ -20,30 +21,31 @@ export const createVariant = async (req, res) => {
         message: "SKU and price are required!",
         success: false,
       });
-    const isProductExist = await productModel.findById(productId)
-    if(!isProductExist) return res.status(404).send({
+    const isProductExist = await productModel.findById(productId);
+    if (!isProductExist)
+      return res.status(404).send({
         message: "Product not found!",
-        success: false
+        success: false,
       });
-      let variantImage;
-    if(req.file) {
-        variantImage = {
-            url:req.file.path,
-            publicId:uuid(),
-            altText:`${req.file.filename} variant Image`
-        }
+    let variantImage;
+    if (req.file) {
+      variantImage = {
+        url: req.file.path,
+        publicId: uuid(),
+        altText: `${req.file.filename} variant Image`,
+      };
     }
     const variant = await variantModel.create({
-        product:productId,
-        sku,
-        price,
-        compareAtPrice,
-        costPrice,
-        currency,
-        isDefault,
-        status,
-        images:variantImage,
-    })
+      product: productId,
+      sku,
+      price,
+      compareAtPrice,
+      costPrice,
+      currency,
+      isDefault,
+      status,
+      images: variantImage,
+    });
     return res.status(201).send({
       message: "Variant created successfully!",
       success: true,
@@ -54,6 +56,53 @@ export const createVariant = async (req, res) => {
     return res.status(500).send({
       message: "Error while creating variant!",
       success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const getAllVariants = async (req, res) => {
+  try {
+    const { page = 1, limit = 10, product, sku } = req.query;
+    const skip = (page - 1) * limit;
+    const cacheKey = `variants:${page}:${limit}:${product || ""}:${sku || ""}`;
+    const cachedVariants = await redis.get(cacheKey);
+    if (cachedVariants)
+      return res.status(200).send({
+        message: "Varient fetched from cache",
+        success: true,
+        variants: JSON.parse(cachedVariants),
+      });
+
+    // filter
+    const filter = {};
+    if (product) {
+      filter.product = product;
+    }
+
+    if (sku) {
+      filter.sku = { $regex: sku, $options: "i" };
+    }
+    const variants = await variantModel
+      .find(filter)
+      .populate("product")
+      .skip(skip)
+      .limit(Number(limit).sort({ createdAt: -1 }));
+    const totalVariants = await variantModel.countDocuments(filter);
+    await redis.set(cacheKey, JSON.stringify(variants), "EX", 300);
+    return res.status(200).send({
+      success: true,
+      message: "All variants fetched successfully",
+      totalVariants,
+      currentPage: Number(page),
+      totalPages: Math.ceil(totalVariants / limit),
+      variants,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Error while fetching variants",
       error: error.message,
     });
   }
