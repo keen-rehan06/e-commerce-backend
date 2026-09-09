f4a0d0607836ec84df5da7c4adf776c5f337e3ce
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-09-09 20:36:16 +0530

    create controllers of add to cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,5 +1,7 @@
 import { variantModel } from "../models/variant.model.js";
+import { inventoryModel } from "../models/inventory.model.js";
 import { cartModel } from "../models/cart.model.js";
+import redis from "../config/redis/redis.js";
 
 export const addToCart = async (req, res) => {
   try {
@@ -88,3 +90,119 @@ export const addToCart = async (req, res) => {
     });
   }
 };
+
+export const getMyCart = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const cacheKey = `cart:${userId}`;
+    const cachedData = await redis.get(cacheKey);
+    if (cachedData)
+      return res.status(200).send({
+        message: "cart fetched from cache.",
+        success: true,
+        ...JSON.parse(cachedData),
+      });
+    const cart = await cartModel
+      .findOne({ user: userId })
+      .populate("items.product")
+      .populate(`items.variant`);
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found",
+      });
+    }
+    await redis.set(cacheKey, JSON.stringify(cart), "EX", 300);
+    return res
+      .status(200)
+      .send({ success: true, message: "Cart fetched successfully", cart });
+  } catch (error) {
+    console.log(error.message);
+     return res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
+
+export const updateMyCart = async (req,res) => {
+  try {
+    const {variantId} = req.params;
+    const { quantity } = req.body;
+    const userId = req.user.id;
+    const cacheKey = `cart:${userId}`;
+    if(!quantity || quantity < 1) {
+      return res.status(400).json({
+        success: false,
+        message: "Quantity must be at least 1",
+      });
+    }
+    const cart = await cartModel.findOne({
+      user: userId,
+    });
+
+     if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found",
+      });
+    }
+    const cartItem = cart.items.find((item) => item.variant.toString() === variantId);
+    if (!cartItem) {
+      return res.status(404).json({
+        success: false,
+        message: "Item not found in cart",
+      });
+    }
+    
+    const inventory = inventoryModel.findOne({
+      variant:variantId,
+    });
+
+    if(!inventory) 
+       return res.status(404).json({
+        success: false,
+        message: "Inventory not found",
+      });
+      const availableQuantity = inventory.quantity - inventory.reservedQuantity;
+
+      if(quantity > availableQuantity) return res.status(400).json({
+        success: false,
+        message: "Insufficient stock",
+      });
+      await redis.del(cacheKey);
+      cartItem.quantity = quantity;
+      await cart.save();
+      return res.status(200).send({
+        success:true,
+        message:"cart item updated successsully!",
+        cart
+      })
+      
+      } catch (error) {
+    console.log(error.message)
+    return res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+}
+
+export const removeCartItem = async (req,res) => {
+  try {
+    const {variantId} = req.params.id;
+    const userId = req.user.id;
+    const cart = await cartModel.findOne({
+      user:userId
+    });
+    if(!cart) return res.status(404).send({message:"Message not found!",success:false})
+
+   // Item cart me hai ya nahi check karo
+   const itemExists = cart.items.some(
+    (item) => item.variant.toString() === variantId
+   )
+    
+  } catch (error) {
+    
+  }
+}
