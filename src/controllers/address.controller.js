ece9ccd8e747227d0b5cd43c15a1c6d74c295b8e
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-09-11 19:57:39 +0530

    refactor getMyAddress and add getSingleAddress controller with caching support

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -63,21 +63,66 @@ export const addAddress = async (req, res) => {
   }
 };
 
-export const getMyAddress = async (req,res) => {
+export const getMyAddress = async (req, res) => {
   try {
     const userId = req.user.id;
     const cacheKey = `address:${userId}`;
     const cachedData = await redis.get(cacheKey);
-    if(cachedData) return res.status(200).send({message:"address fetched from redis.",...JSON.parse(cacheKey),success:true});
-    const addresses = await addressModel.find({user:userId}).sort({ isDefault: -1, createdAt: -1});
-    await redis.set(cacheKey,...JSON.stringify,"EX",300);
-    return res.status(200).send({message:"address fetched from db",success:true,addresses});
+    if (cachedData)
+      return res.status(200).send({
+        message: "address fetched from redis.",
+        ...JSON.parse(cacheKey),
+        success: true,
+      });
+    const addresses = await addressModel
+      .find({ user: userId })
+      .sort({ isDefault: -1, createdAt: -1 });
+    await redis.set(cacheKey, ...JSON.stringify, "EX", 300);
+    return res
+      .status(200)
+      .send({ message: "address fetched from db", success: true, addresses });
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
     return res.status(500).json({
       success: false,
       message: "Failed to get addresses",
       error,
     });
   }
-}
+};
+
+export const getSingleAddress = async (req, res) => {
+  try {
+    const addressId = req.params.id;
+    const cacheKey = `getsingleaddress:${addressId}`;
+    const cachedData = await redis.get(cacheKey);
+    if (cachedData)
+      return res.status(200).send({
+        message: "Address fetched from cache.",
+        success: true,
+        ...JSON.parse(cachedData),
+      });
+    const address = await addressModel.findOne({
+      _id: addressId,
+      user: req.user.id,
+    });
+    if (!address)
+      return res.status(404).json({
+        success: false,
+        message: "Address not found",
+      });
+    await redis.set(cacheKey, ...JSON.stringify(address), "EX", 300);
+
+    return res.status(200).json({
+      success: true,
+      message: "Address fetched successfully",
+      address,
+    });
+  } catch (error) {
+    console.error("Get Single Address Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
