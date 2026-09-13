60e524852299bc9e1389b2796bdb96b412bb3147
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-09-13 21:08:23 +0530

    create a payment controllers

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,88 @@
+import razorpay from "../config/payment/razorpay.payment.js";
+import { paymentModel } from "../models/payment.model.js";
+
+export const createPaymentOrder = async (req, res) => {
+  try {
+    const { amount } = req.body;
+    if (!amount || amount <= 0)
+      return res
+        .status(401)
+        .send({ message: "Valid amount is required", success: false });
+    const options = {
+      amount: Math.round(amount * 100),
+      currency: "INR",
+      receipt: `receipt_${Date.now()}`,
+    };
+    const razorpayOrder = await razorpay.orders.create(options);
+    const payment = await paymentModel.create({
+      user: req.user.id,
+      amount,
+      currency: "INR",
+      status: "PENDING",
+      razorpayOrderId: razorpayOrder.id,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Payment order created successfully",
+      data: {
+        paymentId: payment._id,
+        razorpayOrderId: razorpayOrder.id,
+        amount: razorpayOrder.amount,
+        currency: razorpayOrder.currency,
+      },
+    });
+  } catch (error) {
+    console.error("Create Payment Order Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to create payment order",
+    });
+  }
+};
+
+export const verifyPayment = async (req,res) => {
+  try {
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature
+    } = req.body;
+    if (
+      !razorpay_order_id ||
+      !razorpay_payment_id ||
+      !razorpay_signature
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Payment details are required",
+      });
+    }
+    const body = `${razorpay_order_id} | ${razorpay_payment_id}`;
+    const expectedSigniture = crypto
+    .createHmac("sha256",process.env.RAZORPAY_SECRET_KEY)
+    .update(body)
+    .digest("hex") 
+    if(expectedSigniture !== razorpay_signature) return res.status(400).send({message:"Invaid payment signiture!",success:false});
+    const payment = await paymentModel.findOne({
+      razorpayOrderId:razorpay_order_id,
+      user:req.user._id
+    })
+    if(!payment) return res.status(404).send({message:"Payment record not found!",success:false})
+    razorpay.razorpayPaymentId = razorpay_payment_id;
+    razorpay.razorpaySignature = razorpay_signature;
+    razorpay.status = "SUCCESS";
+    await payment.save();
+     return res.status(200).json({
+      success: true,
+      message: "Payment verified successfully",
+      data: payment,
+    });
+  } catch (error) {
+    console.log(error.message);
+      return res.status(500).json({
+      success: false,
+      message: "Payment verification failed",
+      error
+    });
+  }
+}
