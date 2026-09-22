5682ef9871f74d8eb03c325cc7c724602ccf349b
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-09-22 16:57:58 +0530

    feat: add review model and create review API

--- src/controllers/reviews.controller.js
+++ src/controllers/reviews.controller.js
@@ -1,14 +1,73 @@
+import { reviewModel } from "../models/reviews.model.js";
+import { productModel } from "../models/product.model.js";
+import { orderModel } from "../models/order.model.js";
 
-export const createReview = async(req,res) => {
-    try {
-      const productId = req.params.id;
-      const {orderId,rating,comment} = req.body;
-      const userId = req.user.id;
-      
-      if(!orderId || !rating || !comment) return res.status(401).send({message:"All fields are required!",success:false});
+export const createReview = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const { orderId, rating, comment } = req.body;
+    const userId = req.user.id;
 
-      const product = await productModel.findById(producId);
-    } catch (error) {
-        
-    }
-}
+    if (!orderId || !rating || !comment)
+      return res
+        .status(401)
+        .send({ message: "All fields are required!", success: false });
+
+    const product = await productModel.findById(productId);
+    if (!product)
+      return res
+        .status(404)
+        .send({ message: "Product not found!", success: false });
+    const order = await orderModel.findOne({
+      _id: orderId,
+      user: userId,
+      orderStatus: "DELIVERED",
+    });
+    if (!order)
+      return res.status(404).send({
+        message: "You can review only delivered orders",
+        success: false,
+      });
+    const purchasedProduct = order.items.some(
+      (item) => item.product.toString() === productId,
+    );
+    if (!purchasedProduct)
+      return res
+        .status(401)
+        .send({
+          message: "You have not purchased this product",
+          success: false,
+        });
+
+    const alreadyReviewed = await reviewModel.findOne({
+      user: userId,
+      product: productId,
+    });
+
+    if (alreadyReviewed)
+      return res.status(401).send({
+        message: "You have already reviewed this product",
+        success: false,
+      });
+
+    const review = await reviewModel.create({
+      user: userId,
+      product: productId,
+      order: orderId,
+      rating,
+      comment,
+    });
+    return res.status(201).send({
+      message: "review created successfully!",
+      success: true,
+      review,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+      error,
+    });
+  }
+};
