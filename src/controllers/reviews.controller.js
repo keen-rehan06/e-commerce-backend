ca5b54746f65f0941ab75969f7f3b5dff72e57bd
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-09-22 18:19:34 +0530

    feat: add review model and product review APIs

--- src/controllers/reviews.controller.js
+++ src/controllers/reviews.controller.js
@@ -1,6 +1,7 @@
 import { reviewModel } from "../models/reviews.model.js";
 import { productModel } from "../models/product.model.js";
 import { orderModel } from "../models/order.model.js";
+import redis from "../config/redis/redis.js";
 
 export const createReview = async (req, res) => {
   try {
@@ -32,12 +33,10 @@ export const createReview = async (req, res) => {
       (item) => item.product.toString() === productId,
     );
     if (!purchasedProduct)
-      return res
-        .status(401)
-        .send({
-          message: "You have not purchased this product",
-          success: false,
-        });
+      return res.status(401).send({
+        message: "You have not purchased this product",
+        success: false,
+      });
 
     const alreadyReviewed = await reviewModel.findOne({
       user: userId,
@@ -67,6 +66,49 @@ export const createReview = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Something went wrong!",
+      error,
+    });
+  }
+};
+
+export const getProductReviews = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const cacheKey = `reviews:${productId}`;
+    const cacheData = await redis.get(cacheKey);
+    if (cacheData)
+      return res.status(200).send({
+        message: "Reviews fetched successfully!",
+        success: true,
+        source: "redis",
+        reviews: JSON.parse(cacheData),
+      });
+    const product = await productModel.findById(productId);
+    if (!product)
+      return res
+        .status(404)
+        .send({ message: "product not found!", success: false });
+
+    const reviews = await reviewModel
+      .find({ product: productId })
+      .populate("user", "name")
+      .sort({ createdAt: -1 });
+
+    if (reviews.length < 1)
+      return res.status(200).send({ message: "No reviews!", success: true });
+
+    await redis.set(cacheKey, ...JSON.stringify(reviews), "EX", 300);
+    return res.status(200).send({
+      message: "reviews fetched successfully!",
+      success: true,
+      reviewsLength: reviews.length,
+      reviews,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
       error,
     });
   }
